097dfc936051ed6f34f810c62573887b5ef28fe1
w135153feng73 <583446647@qq.com>
2016-07-27 18:10:31 +0800

    [new]消息中心

---
+++ app/src/main/java/com/ebtang/ebtangebook/view/setting/MessageCenterActivity.java
@@ -0,0 +1,26 @@
+package com.ebtang.ebtangebook.view.setting;
+
+import android.view.View;
+
+import com.ebtang.ebtangebook.app.BaseActivity;
+
+/**
+ * Created by fengzongwei on 2016/7/25 0025.
+ */
+public class MessageCenterActivity extends BaseActivity {
+
+    @Override
+    public void initView() {
+
+    }
+
+    @Override
+    public void initData() {
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
---
+++ app/src/main/java/com/ebtang/ebtangebook/view/setting/adapter/MessageAdapter.java
@@ -0,0 +1,42 @@
+package com.ebtang.ebtangebook.view.setting.adapter;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import java.util.List;
+
+/**
+ * Created by dell on 2016/7/27 0027.
+ */
+public class MessageAdapter extends BaseAdapter {
+
+    private Context context;
+    private List<Object> list;
+
+    public MessageAdapter(Context context,List<Object> list){
+        this.context = context;
+        this.list = list;
+    }
+
+    @Override
+    public int getCount() {
+        return list.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+}
